370cef7745db8ef8a40bcbd3a493cac574d5e752
Jacek Ostapczuk <112418542+JacekOstapczuk@users.noreply.github.com>
2023-03-24 17:00:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
+  const apiRoot = 'http://tasks-prod-kodilla-tasks-f3ze7z.mo5.mogenius.io:80/v1/tasks';
   const trelloApiRoot = 'http://localhost:8080/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
